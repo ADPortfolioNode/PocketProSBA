3b3a57d9a098efce638d95365195474174449f17
AdPortfolioNode <deofunds@gmail.com>
2025-07-29 10:39:36 -0400

    corrcted conciergechat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Login from './Login';
 import Register from './Register';
 import ForgotPassword from './ForgotPassword';
-import ConciergeChat from './ConciergeChat';
+import ConciergeChat from './components/ConciergeChat'
 import ConciergeGreeting from './components/ConciergeGreeting';
 import MainLayout from './components/MainLayout';
 
